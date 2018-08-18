fead3faad4f9b08e901e7ad4902fcfcb9a10143e
Timothy Jarman <tim_jarman@me.com>
2018-08-19 00:25:43 +0100

    Hello World

---
+++ slim/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$app = new Slim\App();
+
+$app->get('/', function ($request, $response, $args) {
+    return $response->getBody()->write("Hello World!");
+});
+
+$app->run();
